faafae57ec253051ee04671baa95b0a9d35facde
rhenando <rhenandos@hotmail.com>
2026-08-23 19:34:30 +0800

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  deploymentId: process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 32),
   serverExternalPackages: ["pdf-parse", "@napi-rs/canvas"],
   images: {
     remotePatterns: [
